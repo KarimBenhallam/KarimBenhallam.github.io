187cea678f47554ae41b5ec0dfd92c3a55eb3efc
KarimBenhallam <karimbenhallam21@gmail.com>
2023-10-20 21:51:44 -0400

    fix display on edge and chrome

--- my-page/src/app-component/home.tsx
+++ my-page/src/app-component/home.tsx
@@ -12,22 +12,18 @@ const Home = () => {
     const p4= getTextFromJSON(context?.language!, "home_content.p4");
 
     return (
-        <div className='w-5 left-50 sticky mx-auto'>
-            <img src="/logo1.ico" alt="Logo" />
-             <h1>{h1}</h1>
-            <p>
-               {p1}
-            </p>
-            <p>
-               {p2}
-            </p>
-            <p>
-               {p3}
-            </p>
-            <p>
-               {p4}
-            </p>
-        </div>
+      // this line was needed so the content was centered in variuous browsers
+      //the previous version only worked on firefox
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <div className='w-5 sticky mx-auto'>
+        <img src="/logo1.ico" alt="Logo" />
+        <h1>{h1}</h1>
+        <p>{p1}</p>
+        <p>{p2}</p>
+        <p>{p3}</p>
+        <p>{p4}</p>
+      </div>
+    </div>
     );
 };
 
